73a56cd436bd1cd3e9452257b31a0d52188c8c50
Sotiris Tsiamouris <tsot85@gmail.com>
2019-06-14 00:08:20 +0300

    materialize css, react routes, fetch user action

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Header from "./Header";
 
-const App = () => {
-  return <div>Hey There</div>;
+const Dashboard = () => {
+  return <h2>Dashboard </h2>;
+};
+const SurveyNew = () => {
+  return <h2>SurveyNew </h2>;
+};
+const Landing = () => {
+  return <h2>Landing </h2>;
 };
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);

6ae80765075810ef240ec52d7fadd10fed2bbe75
Sotiris Tsiamouris <tsot85@gmail.com>
2019-07-03 01:07:59 +0300

    webhook complete

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -9,8 +9,12 @@ module.exports = survey => {
             <p>Please answer the following questions:</p>
             <p>${survey.body}</p>
             <div>
-                <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
-                <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+                <a href="${keys.redirectDomain}/api/surveys/${
+    survey.id
+  }/yes">Yes</a>
+                <a href="${keys.redirectDomain}/api/surveys/${
+    survey.id
+  }/no">No</a>
             </div>
         </div>
     </body>
